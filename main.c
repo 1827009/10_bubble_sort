62d5199c1fbd21f4385968a0bd38ffc6cd7d06c9
1827009 <43544451+1827009@users.noreply.github.com>
2018-12-02 21:37:55 +0900

    Update main.c

--- main.c
+++ main.c
@@ -4,7 +4,23 @@ int main(int argc, char *argv[])
 {
 char str[7];
     
+    
+    int i, j, temp;
 
+  for (i = 0; i < (argc - 1); i++) {
+    for (j = (argc - 1); j > i; j--) {
+       
+      if (argc[j-1] > argc[j]) {
+        temp = argc[j-1];
+        argc[j-1] = argc[j];
+        argc[j] = temp;
+          printf(" ");
+           } printf("%s", argc[i]);
+    }
+  }
+    while(fgets(str, sizeof(str), stdin)){
+        printf("%s", str);
+    }
 
     return 0;
 }
